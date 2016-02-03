9330d3ebdbfcac6ad063c2a7260a2162dd652544
sakib717 <sakibsabbir7@gmail.com>
2016-02-03 16:17:04 -0500

    Added some texts to the html pages

--- server.js
+++ server.js
@@ -5,11 +5,23 @@ var app = express();
 var port = process.env.port || 3000;
 //Main Route
 app.get('/', function (req, res, next) {
-    res.sendFile(path.join(__dirname, "Public", "index.html"));
+    res.sendFile(path.join(__dirname, "Public", "home.html"));
 });
-//Route Info Page
+//Route Projects Page
 app.get('/projects', function (req, res) {
     res.sendFile(path.join(__dirname, "Public", "projects.html"));
+});
+//Route About Me Page
+app.get('/aboutme', function (req, res) {
+    res.sendFile(path.join(__dirname, "Public", "aboutme.html"));
+});
+//Route Services Page
+app.get('/services', function (req, res) {
+    res.sendFile(path.join(__dirname, "Public", "services.html"));
+});
+//Route Contact Page
+app.get('/contact', function (req, res) {
+    res.sendFile(path.join(__dirname, "Public", "contact.html"));
 });
 app.listen(port, function () {
     console.log("Sakib's Website Server Has Started On Port: " + port);
